fd4f277eb504be0fcd69b839d1f497740501a743
Alex Vest <alex.vest@storageos.com>
2018-11-23 14:55:45 +0000

    Upload enable-lio.sh and run in foreground. Only pull storageos container in background

--- provisioning-storage/assets/000-install-storageos.sh
+++ provisioning-storage/assets/000-install-storageos.sh
@@ -1,8 +1,6 @@
-docker -H host01:2345 run --name enable_lio --privileged --rm --cap-add=SYS_ADMIN -v /lib/modules:/lib/modules -v /sys:/sys:rshared storageos/init:0.1
-
-docker -H host02:2345 run --name enable_lio --privileged --rm --cap-add=SYS_ADMIN -v /lib/modules:/lib/modules -v /sys:/sys:rshared storageos/init:0.1
-
-docker -H host03:2345 run --name enable_lio --privileged --rm --cap-add=SYS_ADMIN -v /lib/modules:/lib/modules -v /sys:/sys:rshared storageos/init:0.1
+ssh root@host01 '/opt/enable-lio.sh'
+ssh root@host02 '/opt/enable-lio.sh'
+ssh root@host03 '/opt/enable-lio.sh'
 
 CLUSTER_ID=$(ssh root@host01 "storageos cluster create")
 
@@ -12,5 +10,3 @@ docker -H host02:2345 run -d --name storageos -e HOSTNAME=host02 -e ADVERTISE_IP
 
 docker -H host03:2345 run -d --name storageos -e HOSTNAME=host03 -e ADVERTISE_IP=[[HOST3_IP]] -e JOIN=$CLUSTER_ID --net=host --pid=host --privileged --cap-add SYS_ADMIN --device /dev/fuse -v /var/lib/storageos:/var/lib/storageos:rshared -v /run/docker/plugins:/run/docker/plugins -v /sys:/sys storageos/node:1.0.0 server
 
-scp -p ~/.ssh/id_rsa root@host01:/root/.ssh
-ssh root@host01 "echo [[HOST2_IP]] host02 >> /etc/hosts && echo [[HOST3_IP]] host03 >> /etc/hosts"

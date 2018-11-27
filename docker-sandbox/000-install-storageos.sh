bcc41e2471eeda83b4d05b835ea7cdaf9ca3dabb
Alex Vest <alex.vest@storageos.com>
2018-11-27 16:27:35 +0000

    Tweak cli version

--- docker-sandbox/000-install-storageos.sh
+++ docker-sandbox/000-install-storageos.sh
@@ -13,4 +13,4 @@ docker -H host03:2345 run -d --name storageos -e LOG_LEVEL=debug -e HOSTNAME=hos
 scp -p ~/.ssh/id_rsa root@host01:/root/.ssh
 ssh root@host01 "echo [[HOST2_IP]] host02 >> /etc/hosts && echo [[HOST3_IP]] host03 >> /etc/hosts"
 
-ssh root@host01 "curl -sSLo storageos https://github.com/storageos/go-cli/releases/download/1.0.1/storageos_linux_amd64 && chmod +x storageos && sudo mv storageos /usr/local/bin/ && export STORAGEOS_USERNAME=storageos STORAGEOS_PASSWORD=storageos STORAGEOS_HOST=[[HOST_IP]]"
+ssh root@host01 "curl -sSLo storageos https://github.com/storageos/go-cli/releases/download/1.0.0/storageos_linux_amd64 && chmod +x storageos && sudo mv storageos /usr/local/bin/ && export STORAGEOS_USERNAME=storageos STORAGEOS_PASSWORD=storageos STORAGEOS_HOST=[[HOST_IP]]"

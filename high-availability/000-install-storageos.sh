ccc677776c80047e4a2dd100eafd8a18656c5458
André Lucas <andre.lucas@storageos.com>
2018-12-03 11:29:29 +0000

    Slight refactor

--- high-availability/000-install-storageos.sh
+++ high-availability/000-install-storageos.sh
@@ -3,12 +3,36 @@ ssh root@host02 '/opt/enable-lio.sh'
 ssh root@host03 '/opt/enable-lio.sh'
 
 CLUSTER_ID=$(ssh root@host01 "storageos cluster create")
+IMAGE_ID=814fc6d-dev2918-09b50a5c
 
-docker -H host01:2345 run -d --name storageos -e LOG_LEVEL=DEBUG -e HOSTNAME=host01 -e ADVERTISE_IP=[[HOST_IP]] -e JOIN=$CLUSTER_ID --net=host --pid=host --privileged --cap-add SYS_ADMIN --device /dev/fuse -v /sys:/sys -v /var/lib/storageos:/var/lib/storageos:rshared -v /run/docker/plugins:/run/docker/plugins soegarots/node:814fc6d-dev2918-09b50a5c  server
-
-docker -H host02:2345 run -d --name storageos -e LOG_LEVEL=DEBUG -e HOSTNAME=host02 -e ADVERTISE_IP=[[HOST2_IP]] -e JOIN=$CLUSTER_ID --net=host --pid=host --privileged --cap-add SYS_ADMIN --device /dev/fuse -v /sys:/sys -v /var/lib/storageos:/var/lib/storageos:rshared -v /run/docker/plugins:/run/docker/plugins soegarots/node:814fc6d-dev2918-09b50a5c server
-
-docker -H host03:2345 run -d --name storageos -e LOG_LEVEL=DEBUG -e HOSTNAME=host03 -e ADVERTISE_IP=[[HOST3_IP]] -e JOIN=$CLUSTER_ID --net=host --pid=host --privileged --cap-add SYS_ADMIN --device /dev/fuse -v /sys:/sys -v /var/lib/storageos:/var/lib/storageos:rshared -v /run/docker/plugins:/run/docker/plugins soegarots/node:814fc6d-dev2918-09b50a5c server
+for n in 01 02 03; do
+    case "$n" in
+    01)
+        ip=[[HOST_IP]]
+        ;;
+    02)
+        ip=[[HOST2_IP]]
+        ;;
+    03)
+        ip=[[HOST3_IP]]
+        ;;
+    *)
+        echo "Unnknown host $h" >&2
+        exit 1
+        ;;
+    esac
+    docker "-H host$n":2345 run -d --name storageos \
+        -e LOG_LEVEL=DEBUG\
+        -e HOSTNAME="host$n" \
+        -e ADVERTISE_IP="$ip" \
+        -e JOIN="$CLUSTER_ID" \
+        --net=host --pid=host --privileged --cap-add SYS_ADMIN \
+        --device /dev/fuse \
+        -v /sys:/sys \
+        -v /var/lib/storageos:/var/lib/storageos:rshared \
+        -v /run/docker/plugins:/run/docker/plugins \
+        "soegarots/node:$IMAGE_ID"  server
+done
 
 scp -p ~/.ssh/id_rsa root@host01:/root/.ssh
 ssh root@host01 "echo [[HOST2_IP]] host02 >> /etc/hosts && echo [[HOST3_IP]] host03 >> /etc/hosts"
